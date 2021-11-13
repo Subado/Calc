f3d661236a9eb867aacdb5d83db2f4f85a6633f3
Subado <redcatgamer1980@gmail.com>
2021-11-13 22:40:18 +0400

    Fix bug in genRandStrNum()

--- source/main.cpp
+++ source/main.cpp
@@ -6,14 +6,14 @@
 std::string genRandStrNum()
 {
 	std::string str{};
-
-	for (size_t i = 0; i < rand() % 15 + 1; i++)
+	size_t size = rand() % 15 + 1;
+	for (size_t i = 0; i < size; i++)
 	{
 		str += 48 + rand() % 10;
 
 		if (!((str[i] - 48) && i) && !((str[i] - 48) || i))
 		{
-			str[i] = rand() % 9 + 1;
+			str[i] = 48 + rand() % 9 + 1;
 		}
 	}
 
@@ -22,12 +22,13 @@ std::string genRandStrNum()
 
 int main()
 {
-	srand(time(0));
+    srand(time(0));
+	std::cout << '\n' << "sead = " << time(0) << "\n\n";
 
 	std::string s1{ genRandStrNum() }, s2{ genRandStrNum() };
 	BigInteger n1(s1), n2(s2);
 
-	std::cout << '\n' << s1 << '\n' << n1 << "\n\n" <<
-		s2 << '\n' << n2 << '\n';
+	std::cout << "First num:\n" << s1 << '\n' << n1 << "\n\nSecond num:\n" <<
+        s2 << '\n' << n2 << '\n';
 	return 0;
 }
